825774e8fe44922b044a80bb1828fdb22f7c54b2
Rayan Levert <rayanlevert@msn.com>
2021-01-28 12:24:03 +0100

    Création de base du projet

---
+++ site-critique-projet/src/main/java/fr/dawan/sitecritiqueprojet/SiteCritiqueProjetApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.sitecritiqueprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteCritiqueProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteCritiqueProjetApplication.class, args);
+	}
+
+}
