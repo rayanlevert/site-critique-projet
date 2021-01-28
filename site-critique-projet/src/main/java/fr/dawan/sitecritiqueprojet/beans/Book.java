322bdb15501c0cefb0f2f59ac3dc679c68fdb783
Rayan Levert <rayanlevert@msn.com>
2021-01-28 15:36:20 +0100

    Entity Book

---
+++ site-critique-projet/src/main/java/fr/dawan/sitecritiqueprojet/beans/Book.java
@@ -0,0 +1,72 @@
+package fr.dawan.sitecritiqueprojet.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Column(name = "title", nullable = false)
+    private String title;
+    
+    @Column(name = "author", nullable = false)
+    private String author;
+    
+    @Column(name = "genre", nullable = false)
+    private String genre;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, String genre) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public Book(long id, String title, String author, String genre) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }    
+}
