6f588977141de1b348b786aa80f1c877b50208e7
Rayan Levert <rayanlevert@msn.com>
2021-01-28 13:56:06 +0100

    Création du squelette du projet

--- site-critique-projet/src/test/java/fr/dawan/sitecritiqueprojet/SiteCritiqueProjetApplicationTests.java
+++ site-critique-projet/src/test/java/fr/dawan/sitecritiqueprojet/SiteCritiqueProjetApplicationTests.java
@@ -1,13 +1,13 @@
 package fr.dawan.sitecritiqueprojet;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SiteCritiqueProjetApplicationTests {
+public class SiteCritiqueProjetApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
