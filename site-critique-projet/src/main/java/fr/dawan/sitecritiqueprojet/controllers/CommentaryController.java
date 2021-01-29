26ef4f7589e90f16920f917c5538fa2ab72fa24b
Victor Andre <andre.victorfr60@gmail.com>
2021-01-29 13:44:00 +0100

    CRUD CONTROLLER

--- site-critique-projet/src/main/java/fr/dawan/sitecritiqueprojet/controllers/CommentaryController.java
+++ site-critique-projet/src/main/java/fr/dawan/sitecritiqueprojet/controllers/CommentaryController.java
@@ -3,8 +3,12 @@ package fr.dawan.sitecritiqueprojet.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +36,24 @@ public class CommentaryController {
 	@GetMapping(value = "/review/{id}", produces = "application/json")
 	public List<Commentary> getCommentsByReviewId(@PathVariable("id") long id){
 		return commentaryService.findCommentaryByReviewId(id);
+	}
+	
+	@PostMapping(consumes = "application/json", produces = "application/json")
+	public Commentary createCommentary(@RequestBody Commentary c) {
+		return commentaryService.saveOrUpdate(c);
+	}
+	@PutMapping(consumes = "application/json", produces = "application/json")
+	public Commentary updateCommentary(@RequestBody Commentary c) {
+		return commentaryService.saveOrUpdate(c);
+	}
+	@DeleteMapping(value = "/commentary/{id}", produces = "text/plain")
+	public String deleteReviewById(@PathVariable("id") long id) {
+		try {
+			commentaryService.deleteById(id);
+			return "Suppr ok";
+		}catch(Exception e) {
+			e.printStackTrace();
+			return "Erreur : " + e.getMessage();
+		}
 	}
 }
--- site-critique-projet/src/main/java/fr/dawan/sitecritiqueprojet/controllers/ReviewController.java
+++ site-critique-projet/src/main/java/fr/dawan/sitecritiqueprojet/controllers/ReviewController.java
@@ -3,8 +3,12 @@ package fr.dawan.sitecritiqueprojet.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +36,25 @@ public class ReviewController {
 	@GetMapping(value = "/article/{id}", produces ="application/json")
 	public List<Review> getReviewsByArticleId(@PathVariable("id") long id){
 		return reviewService.findReviewByArticleId(id);
+	}
+	
+	@PostMapping(consumes = "application/json", produces = "application/json")
+	public Review createReview(@RequestBody Review r) {
+		return reviewService.saveOrUpdate(r);
+	}
+	@PutMapping(consumes = "application/json", produces = "application/json")
+	public Review updateReview(@RequestBody Review r) {
+		return reviewService.saveOrUpdate(r);
+	}
+	@DeleteMapping(value = "/review/{id}", produces = "text/plain")
+	public String deleteReviewById(@PathVariable("id") long id) {
+		try {
+			reviewService.deleteById(id);
+			return "Suppr ok";
+		}catch(Exception e) {
+			e.printStackTrace();
+			return "Erreur : " + e.getMessage();
+		}
 	}
 }
 
