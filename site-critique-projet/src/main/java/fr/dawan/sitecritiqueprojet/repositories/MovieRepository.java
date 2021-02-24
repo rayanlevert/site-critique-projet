eda5c8947227a3877d10fdbca2586ed6d868a8fd
Boudin Joey <boudin.joey.pro@gmail.com>
2021-02-24 18:23:18 +0100

    petit up du filtre

--- site-critique-projet/src/main/java/fr/dawan/sitecritiqueprojet/repositories/MovieRepository.java
+++ site-critique-projet/src/main/java/fr/dawan/sitecritiqueprojet/repositories/MovieRepository.java
@@ -14,37 +14,37 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     
     //search with filters
     
-    @Query("SELECT m from Movie m where m.title LIKE %:search% AND m.genre LIKE %:filterValue%")
+    @Query("SELECT m from Movie m where m.title LIKE %:search% AND m.genre LIKE %:filterValue% ORDER BY id DESC")
     List<Movie> searchByGenre(String search, String filterValue);
 
-    @Query("SELECT m from Movie m where m.title LIKE %:search% AND m.realisator = :filterValue")
+    @Query("SELECT m from Movie m where m.title LIKE %:search% AND m.realisator = :filterValue ORDER BY id DESC")
     List<Movie> searchByReal(String search, String filterValue);
 
-    @Query("SELECT m from Movie m where m.title LIKE %:search% AND m.genre LIKE %:filterValue%")
+    @Query("SELECT m from Movie m where m.title LIKE %:search% AND m.genre LIKE %:filterValue% ORDER BY id DESC")
     List<Movie> searchByActor(String search, String filterValue);
 
-    @Query("SELECT m from Movie m where m.title LIKE %:search% AND m.duration = :filterValue")
+    @Query("SELECT m from Movie m where m.title LIKE %:search% AND m.duration = :filterValue ORDER BY id DESC")
     List<Movie> searchByDuration(String search, String filterValue);
 
-    @Query("SELECT m from Movie m where m.title LIKE %:search% AND m.nationality LIKE %:filterValue%")
+    @Query("SELECT m from Movie m where m.title LIKE %:search% AND m.nationality LIKE %:filterValue% ORDER BY id DESC")
     List<Movie> searchByNationality(String search, String filterValue);
     
     
     //custom finds requests
 
-    @Query("SELECT m from Movie m where m.genre = :filterValue")
+    @Query("SELECT m from Movie m where m.genre = :filterValue ORDER BY id DESC")
     List<Movie> findByGenre(String filterValue);
 
-    @Query("SELECT m from Movie m where m.realisator = :filterValue")
+    @Query("SELECT m from Movie m where m.realisator = :filterValue ORDER BY id DESC")
     List<Movie> findByReal(String filterValue);
 
-    @Query("SELECT m from Movie m where m.genre LIKE %:filterValue%")
+    @Query("SELECT m from Movie m where m.genre LIKE %:filterValue% ORDER BY id DESC")
     List<Movie> findByActor(String filterValue);
 
-    @Query("SELECT m from Movie m where m.duration = :filterValue")
+    @Query("SELECT m from Movie m where m.duration = :filterValue ORDER BY id DESC")
     List<Movie> findByDuration(String filterValue);
 
-    @Query("SELECT m from Movie m where m.nationality LIKE %:filterValue%")
+    @Query("SELECT m from Movie m where m.nationality LIKE %:filterValue% ORDER BY id DESC")
     List<Movie> findByNationality(String filterValue);
 
 }
